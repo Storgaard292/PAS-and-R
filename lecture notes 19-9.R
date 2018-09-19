e29e30c8c933d6b26907980dfe69afc5debd4cc2
Christian Storgaard <cskr14@student.aau.dk>
2018-09-19 13:48:51 +0200

    del 1 done

--- lecture notes 19-9.R
+++ lecture notes 19-9.R
@@ -5,4 +5,41 @@ library("mvtnorm")
 dmvnorm(c(1,2,2),mean=rep(0,3),sigma=diag(c(1,1,1)))
 #rmarkdown::render("lecture notes 19-9.R")
 
+#types of code
+
+s=0
+for(i in 1:100){
+  s=s+i
+}
+s
+
+#if-then-else
+x=-3
+if(x<0){
+  signx=-1
+} else {
+  if(x==0){
+    signx=0
+  } else{
+    signx=1
+  }
+}
+signx
+
+signfct=function(x){
+  signx=0
+  if(x<0){
+    signx=-1
+  }
+  if(x>0){
+    signx=1
+  }
+  if(x==0){
+    signx=0
+  }
+  signx
+}
+
+
+signfct(1)
 
